723667889fa47d21ca4d3d23858774a4cab9bcc4
Miguel <miguel@admins-Mac-mini.local>
2020-06-20 16:01:20 -0300

    DoIt v1.0.1 Icon improved; Initial page design`s better; Icon and app name added to Initital page.

--- src/pages/InitialPage.js
+++ src/pages/InitialPage.js
@@ -13,28 +13,32 @@ import Icon from 'react-native-vector-icons/AntDesign';
 export default function InitialPage({ navigation }) {
   return (
     <View style={styles.container}>
-      <StatusBar backgroundColor="#FFF" barStyle="dark-content" />
+      <StatusBar backgroundColor="#ededed" barStyle="dark-content" />
       <View style={styles.logo}>
-        <Text>Logo</Text>
-      </View>
-      <View style={styles.chooseView}>
-        <Text style={styles.viewTitle}>Já possui conta!</Text>
-        <TouchableOpacity
-          style={styles.touch}
-          onPress={() => navigation.navigate('Login')}>
-          <Text style={styles.touchText}>Entrar</Text>
-          <Icon name="right" size={30} color="#FFF" />
-        </TouchableOpacity>
+        <Icon name="star" size={65} color="#cc6166" />
+        <Text style={styles.logoTitle}>DoIt</Text>
       </View>
 
-      <View style={styles.chooseView}>
-        <Text style={styles.viewTitle}>Não possui uma conta ainda?</Text>
-        <TouchableOpacity
-          style={styles.touch}
-          onPress={() => navigation.navigate('Register')}>
-          <Text style={styles.touchText}>Criar conta</Text>
-          <Icon name="right" size={30} color="#FFF" />
-        </TouchableOpacity>
+      <View style={styles.chooseContainer}>
+        <View style={[styles.chooseView]}>
+          <Text style={styles.viewTitle}>Já possui conta!</Text>
+          <TouchableOpacity
+            style={styles.touch}
+            onPress={() => navigation.navigate('Login')}>
+            <Text style={styles.touchText}>Entrar</Text>
+            <Icon name="right" size={30} color="#FFF" />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.chooseView}>
+          <Text style={styles.viewTitle}>Não possui uma conta ainda?</Text>
+          <TouchableOpacity
+            style={styles.touch}
+            onPress={() => navigation.navigate('Register')}>
+            <Text style={styles.touchText}>Criar conta</Text>
+            <Icon name="right" size={30} color="#FFF" />
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -43,9 +47,20 @@ export default function InitialPage({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#ededed',
+  },
+  chooseContainer: {
+    flex: 1,
     backgroundColor: '#FFF',
+    borderTopLeftRadius: 45,
+    borderTopRightRadius: 45,
+    elevation: 9,
   },
   chooseView: {
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+    flex: 1,
+    backgroundColor: '#FFF',
     padding: 12,
     margin: 8,
     justifyContent: 'space-around',
@@ -66,6 +81,7 @@ const styles = StyleSheet.create({
     borderRadius: 14,
     alignItems: 'center',
     justifyContent: 'space-between',
+    elevation: 3,
   },
   touchText: {
     fontSize: 24,
@@ -74,6 +90,14 @@ const styles = StyleSheet.create({
   },
   logo: {
     height: '50%',
-    backgroundColor: '#ddd',
+    backgroundColor: '#ededed',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoTitle: {
+    color: '#cc6166',
+    fontSize: 65,
+    fontFamily: 'RobotoMono-Bold',
+    marginTop: 20,
   },
 });
